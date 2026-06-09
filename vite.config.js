a10cdd38403fb7de45217efe22a47f53204da669
MarCrivelli <marcoantonio7crivelli@gmail.com>
2026-06-09 19:04:30 -0300

    Teste GithubPages 2

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { host: true },
-  base: 'InstitutoEsperancaSite',
+  base: '/InstitutoEsperancaSite/',
 })
